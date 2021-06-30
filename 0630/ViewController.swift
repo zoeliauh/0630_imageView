486c062270b2d3dd72861efb597c94927a3ef469
zoeliauh <ibm801227@hotmail.com>
2021-06-30 20:28:39 +0800

    Initial Commit

---
+++ 0630/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0630
+//
+//  Created by woanjwu liauh on 2021/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
