9befde03b3b9860860a00afbabcff40dd70f44a1
zoeliauh <82745286+zoeliauh@users.noreply.github.com>
2021-07-03 00:21:15 +0800

    about imageView setting
    imageView setting

--- 0630/ViewController.swift
+++ 0630/ViewController.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var myImageView: UIImageView!
+    
+    var names: [String] = ["cover", "sestra", "twins"]
+    
+    @IBAction func userClick(_ sender: UIButton) {
+        switch sender.tag {
+        case 100:
+            myImageView.image = UIImage.init(named: "orphan black cover")
+            print(names[0])
+        case 200:
+            myImageView.image = UIImage.init(named: "Clones")
+            print(names[1])
+        case 300:
+            myImageView.image = UIImage.init(named: "Helena with Sarah")
+            print(names[2])
+        default:
+            print("nil")
+        }
+    }
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
